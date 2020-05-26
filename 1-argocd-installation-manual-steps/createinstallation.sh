65c3520a152c63d59b30ed9b13c3398417424475
Paolo Carta <ing.pcarta@gmail.com>
2020-05-26 10:32:51 +0200

    introducing argocd self managed, cluster bootstrap and apps of apps pattern

--- 1-argocd-installation-manual-steps/createinstallation.sh
+++ 1-argocd-installation-manual-steps/createinstallation.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-oc create -f 1-argo-ns.yaml
-oc create -f 2-argo-og.yaml
-oc create -f 3-argo-sub.yaml
-oc create -f 4-argo-groups.yaml
+oc apply -f 1-argo-ns.yaml
+oc apply -f 2-argo-og.yaml
+oc apply -f 3-argo-sub.yaml
+
 echo Waiting 30 seconds for ArgoCD Operator to come up, if the following step fails with the
 echo error of \"no matches for kind ArgoCD\" then reload the \"5-argo-cluster.yaml\" file manually
-echo using \"oc create -f 5-argo-cluster.yaml\".
+echo using \"oc apply -f 5-argo-cluster.yaml\".
+
 sleep 30
-oc create -f 5-argo-cluster.yaml
+
+oc apply -f 5-argo-cluster.yaml
+oc apply -f 6-argocd-cluster-admin.yaml
--- 2-demo-namespaces/createnamespaces.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-oc create -f amqstreams-ns.yaml
-oc create -f bookinfo-ns.yaml
-oc create -f codereadyworkspaces-ns.yaml
-oc create -f developerteam1-ns.yaml
-oc create -f developerteam2-ns.yaml
-oc create -f devtools.yaml
-oc create -f istio-system-ns.yaml
-oc create -f metering-ns.yaml
